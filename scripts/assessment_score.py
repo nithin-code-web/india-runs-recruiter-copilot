7d7a1ac0e63eb2af2b78983e61f35c95a4474021
nithin-code-web <nithinbudime@gmail.com>
2026-06-11 10:51:14 +0530

    add assessment score calculation

---
+++ scripts/assessment_score.py
@@ -0,0 +1,24 @@
+def calculate_assessment_score(assessment_scores):
+
+    if not assessment_scores:
+        return 0, 0
+
+    average_score = (
+        sum(assessment_scores.values())
+        / len(assessment_scores)
+    )
+
+    scaled_score = round(average_score / 10)
+
+    return scaled_score, round(average_score, 2)
+
+from extract_candidate_features import candidate_features
+
+assessment_score, average_score = (
+    calculate_assessment_score(
+        candidate_features["assessment_scores"]
+    )
+)
+
+print("Average Assessment:", average_score)
+print("Assessment Score:", assessment_score)
